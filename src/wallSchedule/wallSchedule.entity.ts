1ab2d2b44192df0d4d731f5106f50ac9cc33ff6a
no-support <jwan0131@gmail.com>
2024-06-02 16:34:59 +0900

    define date scalar

---
+++ src/wallSchedule/wallSchedule.entity.ts
@@ -0,0 +1,17 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { Wall } from '../wall/wall.entity';
+
+@ObjectType({ description: '벽 세팅 스케줄' })
+export class WallScheduleEntity {
+  @Field((type) => ID)
+  id: number;
+
+  @Field((type) => Wall)
+  wall: Wall;
+
+  @Field((type) => Date)
+  settingDate: Date;
+
+  @Field((type) => Boolean)
+  isDeleted: boolean;
+}
